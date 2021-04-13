faa2d526400ce31121bdbfaf776f376a9d0b7c3a
mark <mark@localhost.localdomain>
2021-04-13 23:36:03 +0300

    Added ability to add and search for a patient

--- src/personnel/Physician.java
+++ src/personnel/Physician.java
@@ -1,72 +1,92 @@
 package personnel;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Physician {
 
     private int phoneNo;
-
     private String name;
     private String id;
     private String address;
     private String expertise;
+    private String consultationHrs;
+    private String consultationDays;
 
     private String answer;
 
     private String personInfo = "";
 
-    String path1 = System.getProperty("user.dir");
-    String path = path1.substring(0, (path1.length() - 4)) + "\\database\\";
+    String path = System.getProperty("user.dir") + "/src/database/";
+    // String path = path1.substring(0, (path1.length() - 4)) + "/database/";
 
-    Scanner sc = new Scanner(System.in);
+    public Scanner sc = new Scanner(System.in);
 
-    Physician() {
-
+    public Physician() {
+        // actions();
     }
 
-    Physician(String name, String id, String address, int phoneNo) {
+    public Physician(String name, String id, String address, int phoneNo, String expertise, String consultationHrs, String consultationDays) {
         this.name = name;
         this.id = id;
         this.address = address;
         this.phoneNo = phoneNo;
+        this.expertise = expertise;
+        this.consultationHrs = consultationHrs;
+        this.consultationDays = consultationDays;
 
         addToDB("Physician.txt");
     }
 
-    public void newPhysician() {
-        if(name == null) {
-            askDetails();
+    public void actions(int ans) {
+        // int ans = Integer.parseInt(qString("Please select option:\n"
+        // +"1. New Physician.\n"
+        // +"2. Find a physician.\n"
+        // ));
+
+        if (ans == 1) {
+            newPhycisian();
+        } else if (ans == 2) {
+            id = qString("Enter doctor's name or ID");
+            openDB("Physician.txt", id);
+        } else {
+            System.out.println("Enter correct choice (using numbers)");
         }
     }
 
-    public void askDetails() {
+    private void newPhycisian() {
+        System.out.println(path);
         name = qString("Physician's name: ");
-        name = qString("Physician's id: ");
+        id = qString("Physician's id: ");
         address = qString("Physician's address: ");
         phoneNo = Integer.parseInt(qString("Physician's phone Number: "));
+        expertise = qString("Doctor's expertise");
+        consultationDays = qString("Days of the week available (in 'Mon Tue Wed' format): ");
+        consultationHrs = qString("Time of the day available: ");
+
+        addToDB("Physician.txt");
     }
 
-    public String qString(String question) {
+    private String qString(String question) {
         System.out.println(question);
         answer = sc.nextLine();
         return answer;
     }
 
-    public void addToDB(String file) {
+    private void addToDB(String file) {
 
-        if(name == null) {
-            askDetails();
-        }
         try {
             path += file;
 
-            System.out.println(path);
-
             FileWriter fr = new FileWriter(path, true);
 
-            personInfo = "name :: " + name + "\t" + "id :: " + id + "\t" + "address :: "+ address + "\t" + "expertise :: " + expertise + "\t";
+            personInfo = "\tName  :: " + name + "\tID ::  " 
+                + id + "\tAddress   :: " + address + "\tExpertise :: "
+                + expertise + "\t" + "\tPhone :: " + phoneNo + "\tFree on :: "
+                + consultationDays + "\tat :: " + consultationHrs;
             fr.write(personInfo + "\n");
 
             System.out.println("Successfully created user");
@@ -75,5 +95,30 @@ public class Physician {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    private boolean openDB(String file, String id) {
+        path += file;
+        try {
+            sc = new Scanner(new File(path));
+            String str = "";
+            do {
+                try {
+                    str = sc.nextLine();
+                } catch (Exception e) {
+                    System.out.print("Empty database :(   .   ");
+                }
+                if (str.length() > id.length()) {
+                    if (str.contains(id)) {
+                        System.out.println(str);
+                        return true;
+                    }
+                }
+            } while (sc.hasNextLine());
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("User not found");
+        return false;
     }
 }
