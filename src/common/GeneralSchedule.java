8ff235cbdb9122365ef7928a58e9b3266c164ad8
mark <mark@localhost.localdomain>
2021-04-14 10:22:05 +0300

    Added generalschedular, will help in making schedules and booking appointments

---
+++ src/common/GeneralSchedule.java
@@ -0,0 +1,14 @@
+package common;
+
+import myCal.*;
+
+public class GeneralSchedule {
+
+    String[] days = {"Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"};
+    int firstTime = 0000;
+    int lastTime = 2359;
+
+    int startTime;
+    int endTime;
+    
+}
